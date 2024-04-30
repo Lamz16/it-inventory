4008aa933ffaec78cb19449576a6966aa7493981
lazlanrafar <lazlanrafar@gmail.com>
2024-04-30 19:01:42 +0800

    feat(docker): setup docker

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,4 +10,27 @@ class AuthController extends Controller
     {
         return view('pages.auth.login');
     }
+
+    public function authenticate(Request $request)
+    {
+        if (Auth::attempt(['username' => $request->useremail, 'password' => $request->password])) {
+            $request->session()->regenerate();
+
+            $user = User::where('username', $request->useremail)->first();
+            $request->session()->put('user', $user);
+
+            return redirect('/');
+        }
+
+        if (Auth::attempt(['email' => $request->useremail, 'password' => $request->password])) {
+            $request->session()->regenerate();
+
+            $user = User::where('email', $request->useremail)->first();
+            $request->session()->put('user', $user);
+
+            return redirect('/');
+        }
+
+        return back()->with('error', 'Email atau Password salah');
+    }
 }
